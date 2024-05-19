452e834151adeaec1f30701402c7ad3810544354
Beata Stępień <beata.stepie2@gmail.com>
2024-05-19 21:15:02 +0200

    Delete meeting

--- src/main/frontend/src/meetings/MeetingsPage.js
+++ src/main/frontend/src/meetings/MeetingsPage.js
@@ -1,28 +1,39 @@
 import {useState} from "react";
 import NewMeetingForm from "./NewMeetingForm";
 import MeetingsList from "./MeetingsList";
-import { useEffect } from 'react';
-
 
 export default function MeetingsPage({username}) {
     const [meetings, setMeetings] = useState([]);
     const [addingNewMeeting, setAddingNewMeeting] = useState(false);
 
-    function handleNewMeeting(meeting) {
-        const nextMeetings = [...meetings, meeting];
-        setMeetings(nextMeetings);
-        setAddingNewMeeting(false);
+    async function handleNewMeeting(meeting) {
+        const response = await fetch('/api/meetings', {
+            method: 'POST',
+            body: JSON.stringify(meeting),
+            headers: {'Content-Type': 'application/json'}
+        });
+        if (response.ok) {
+            const savedMeeting = await response.json();
+            const nextMeetings = [...meetings, savedMeeting];
+            setMeetings(nextMeetings);
+            setAddingNewMeeting(false);
+        }
     }
 
-    function handleDeleteMeeting(meeting) {
-        const nextMeetings = meetings.filter(m => m !== meeting);
-        setMeetings(nextMeetings);
+    async function handleDeleteMeeting(meeting) {
+        const response = await fetch(`/api/meetings/${meeting.id}`, {
+            method: 'DELETE',
+        });
+        if (response.ok) {
+            const nextMeetings = meetings.filter(m => m.id !== meeting.id);
+            setMeetings(nextMeetings);
+        }
     }
 
     function handleSignIn(meeting) {
         const nextMeetings = meetings.map(m => {
-            if (m === meeting) {
-                m.participants = [...m.participants, username];
+            if (m.id === meeting.id) {
+                return {...m, participants: [...m.participants, username]};
             }
             return m;
         });
@@ -31,41 +42,20 @@ export default function MeetingsPage({username}) {
 
     function handleSignOut(meeting) {
         const nextMeetings = meetings.map(m => {
-            if (m === meeting) {
-                m.participants = m.participants.filter(u => u !== username);
+            if (m.id === meeting.id) {
+                return {...m, participants: m.participants.filter(u => u !== username)};
             }
             return m;
         });
         setMeetings(nextMeetings);
     }
-        async function handleNewMeeting(meeting) {
-            const response = await fetch('/api/meetings', {
-                method: 'POST',
-                body: JSON.stringify(meeting),
-                headers: {'Content-Type': 'application/json'}
-            });
-            if (response.ok) {
-                const nextMeetings = [...meetings, meeting];
-                setMeetings(nextMeetings);
-                setAddingNewMeeting(false);
-            }
-        }
-    useEffect(() => {
-        const fetchMeetings = async () => {
-            const response = await fetch(`/api/meetings`);
-            if (response.ok) {
-                const meetings = await response.json();
-                setMeetings(meetings);
-            }
-        };
-        fetchMeetings();
-    }, []);
+
     return (
         <div>
             <h2>Zajęcia ({meetings.length})</h2>
             {
                 addingNewMeeting
-                    ? <NewMeetingForm onSubmit={(meeting) => handleNewMeeting(meeting)}/>
+                    ? <NewMeetingForm onSubmit={handleNewMeeting}/>
                     : <button onClick={() => setAddingNewMeeting(true)}>Dodaj nowe spotkanie</button>
             }
             {meetings.length > 0 &&
